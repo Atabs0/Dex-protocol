6090c3ee34c8691f8bcfcc82a7027c805ee68569
atabong <tajoacha@gmail.com>
2023-07-21 13:02:00 +0300

    files

---
+++ dexBack/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const Moralis = require("moralis").default;
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/tokenPrice", async (req, res) => {
+
+  const{query} = req;
+
+  const responseOne = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressOne
+  })
+
+  const responseTwo = await Moralis.EvmApi.token.getTokenPrice({
+    address: query.addressTwo
+  })
+
+  const usdPrices ={
+    tokenOne: responseOne.raw.usdPrice,
+    tokenTwo: responseTwo.raw.usdPrice,
+    ratio: responseOne.raw.usdPrice/responseTwo.raw.usdPrice
+  }
+
+  // console.log(responseOne.raw);
+  // console.log(responseTwo.raw);
+  //this below was run on web
+  /*http://localhost:3001/tokenPrice?addressOne=0xdac17f958d2ee523a2206206994597c13d831ec7&addressTwo=0x514910771af9ca656af840dff83e8264ecf986ca*/ 
+
+  return res.status(200).json(usdPrices);
+});
+
+Moralis.start({
+  apiKey: process.env.MORALIS_KEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Listening for API Calls`);
+  });
+});
